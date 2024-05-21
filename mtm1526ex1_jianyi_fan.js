d21caedbea859bfe42ac3ac0d07445dabe11200b
nealfan01 <49001890+nealfan01@users.noreply.github.com>
2024-05-20 21:38:05 -0400

    Add files via upload

---
+++ mtm1526ex1_jianyi_fan.js
@@ -0,0 +1,54 @@
+//Task 1
+// alert( "description of alerted_value: " + alerted_value );
+/*
+a + b
+a - b
+a * b
+a / b
+a % b
+a += b
+a -= b
+a *= b
+a /= b
+a %= b
+a == b
+a != b
+a > b
+a < b
+!a && !c 
+!a || !c 
+*/
+
+let a = 3;
+let b = 5;
+let c;
+
+console.log(`
+let a = 3;
+let b = 5;
+let c;
+-----------------
+a + b =  ${a + b} \n` +
+`a - b =  ${a - b} \n` +
+`a * b =  ${a * b} \n` +
+`a / b =  ${a / b} \n` +
+`a % b =  ${a % b} \n` +
+`a += b =  ${a += b} \n` +
+`a -= b =  ${a -= b} \n` +
+`a *= b =  ${a *= b} \n` +
+`a /= b =  ${a /= b} \n` +
+`a %= b =  ${a %= b} \n`+
+`a == b =  ${a == b} \n`+
+`a != b =  ${a != b} \n`+
+`a > b =  ${a > b} \n` +
+`a < b =  ${a < b} \n` +
+`!a && !c =  ${!a && !c} \n`+
+`!a || !c =  ${!a || !c} \n`);
+
+
+//Task 2
+let first_name = "Jianyi";
+let last_name = "Fan";
+let email = "fan00079@algonquinlive.com";
+let output = `My name is ${first_name} ${last_name}. You can contact me at ${email}.`;
+console.log(output);
